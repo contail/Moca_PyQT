97c6fe3e37e257cd149f8c676741324c8fba9265
contail <qkrtkdwls2121@gmail.com>
2018-08-22 14:44:47 +0900

    divide Ui code

--- feedback.py
+++ feedback.py
@@ -12,17 +12,30 @@ from PyQt5.QtCore import Qt
 class Form(QWidget):
 
     def __init__(self):
+        super(self.__class__, self).__init__()
+        self.setupUi()
+
+        #button hooks
+        self.pb.clicked.connect(lambda : self.get_file_name)
+
+    def setupUi(self):
         QWidget.__init__(self, flags=Qt.Widget)
+        self.resize(480, 320)
         self.setWindowTitle("피드백")
+
         box = QBoxLayout(QBoxLayout.TopToBottom)
         self.lb = QLabel()
         self.lb_1 = QLabel()
-        self.pb = QPushButton("이미지 파일을 선택해주세요")
+
+        self.pb = QPushButton("이미지 파일을 선택해주세요",self)
         box.addWidget(self.lb)
         box.addWidget(self.pb)
         self.setLayout(box)
-        self.pb.clicked.connect(self.get_file_name)
         box.addWidget(self.lb_1)
+
+
+    def start_feedback(self):
+        return 1
 
 
     def get_file_name(self):
@@ -30,7 +43,7 @@ class Form(QWidget):
         filename = QFileDialog.getOpenFileName()
         self.lb.setText(filename[0])
         pixmap = QPixmap(filename[0])
-        pixmap = pixmap.scaledToHeight(240)  # 사이즈가 조정
+        pixmap = pixmap.scaledToHeight(240)
         self.lb_1.setPixmap(pixmap)
 
 if __name__ == "__main__":
